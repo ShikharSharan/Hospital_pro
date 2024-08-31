33577f41fefa2fd10b63f8ead7cb5d0af8fe2dd5
snehal123242 <93393196+snehal123242@users.noreply.github.com>
2024-08-31 21:29:05 +0530

    Update booking_appointement.py

--- booking_appointement.py
+++ booking_appointement.py
@@ -98,9 +98,9 @@ def past_appointments(Patient_ID):
 
 ######################################--MEDICAL---HISTORY##########################################################
 def generate_record_ID():
-    prefix = ''.join(random.choices(string.ascii_uppercase, k=2))
-    digits = ''.join(random.choices(string.digits, k=6))
-    return f"RE{prefix}{digits}"
+    #prefix = .join(random.choices(string.ascii_uppercase, k=2))
+    digits = 'RE'.join(random.choices(string.digits, k=6))
+    return f"{digits}"
 
 def medical_history(Patient_ID,Allergies,Pre_conditions):
     Record_ID = generate_record_ID
@@ -139,4 +139,154 @@ def medical_history(Patient_ID,Allergies,Pre_conditions):
             conn.close()
     
     
+################################LAB-- TEST ###############################
+
+# Test_ID,TEST_NAME,PATIENT_ID,DOCTOR_ID,DATE,TEST_COST
+
+def generate_Test_ID():
+
+    digits = ''.join(random.choices(string.digits, k=6))
+    return f"TE{digits}"
+
+def test_name(TEST_NAME):
+    query = f"select TEST_COST from user_data.LAB_TESTS where TESTS = {TEST_NAME}"
+    config = load_redshift_config
+    try:
+        conn = psycopg2.connect(
+        host = config['host'],
+        port = config['port'],
+        dbname = config['dbname'],
+        user = config['user'],
+        password = config['password']
+        )
+        cursor = conn.cursor()
+        cursor.execute(query)
+        result=cursor.fetchall()   
+        print(result)
+        conn.commit()
+        return result
+        
+    except Exception as e:
+        print(f"Error Inserting data in redshift.{e}")
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+    return 
+def Lab_test(TEST_NAME,PATIENT_ID,DOCTOR_ID,DATE):
     
+    TEST_ID = generate_Test_ID()
+    print(TEST_ID)
+    TEST_NAME = TEST_NAME
+    print(TEST_NAME)
+    PATIENT_ID= PATIENT_ID
+    print(PATIENT_ID)
+    DOCTOR_ID = DOCTOR_ID
+    print(DOCTOR_ID)
+    DATE = DATE
+    print(DATE)
+    TEST_COST = test_name(TEST_NAME)
+    config =  load_redshift_config()
+    try:
+        conn = psycopg2.connect(
+          host = config['host'], 
+          port = config['port'], 
+          dbname = config['dbname'],
+          user = config['user'],
+          password = config['password']
+        )
+        cursor = conn.cursor()
+        insert_lab_test = """
+        INSERT INTO user_data.Lab_test(TEST_ID,TEST_NAME,PATIENT_ID,DOCTOR_ID,DATE,TEST_COST)
+        VALUES(%s,%s,%s,%s,%s,%s)"""
+        cursor.execute(insert_lab_test(TEST_ID,TEST_NAME,PATIENT_ID,DOCTOR_ID,DATE,TEST_COST))
+        
+        conn.commit()
+    except Exception as e:
+        print(f"Error inserting data into redshift:{e}")
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+    
+Lab_test("laboraratory","C1107","D1107","22 August 2024")
+
+###############- LAB- TEST --- DATA LOAD DONE------#############################
+
+
+################ - PRESCRIPTION----#########################################
+
+def generate_prescription_ID():
+    digits =''.join(random.choices(string.digits, k=6))
+    return f"PE{digits}"
+def prescription(Patient_ID,Doctor_ID,Medicine_ID,Dosage,Date):
+    Prescription_ID = generate_prescription_ID()
+    Patient_ID = Patient_ID
+    Doctor_ID = Doctor_ID
+    Medicine_ID = Medicine_ID
+    Dosage = Dosage
+    Date= Date
+
+    config = load_redshift_config()
+    try:
+        conn = psycopg2.connect(
+            host = config['host'],
+            port = config['port'],
+            dbname = config['dbname'],
+            user = config['user'],
+            password = config['password']
+        )
+        cursor = conn.cursor()
+        insert_prescription_details = """
+        INSERT INTO user_data.Prescription( Prescription_ID,Patient_ID,Doctor_ID,Medicine_ID,Dosage,Date)
+        VALUES(%s,%s,%s,%s,%s,%s)"""
+        cursor.execute(insert_prescription_details(Prescription_ID,Patient_ID,Doctor_ID,Medicine_ID,Dosage,Date))
+        conn.commit()
+    except Exception as e:
+        print(f"Error loading data in redshift:{e}")
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+    
+#########PRESCRIPTION LOADED SUCCESSFULLY ##############################
+
+################--BILL-GENERATED--###########################
+
+def generate_bill_ID():
+    digits = ''.join(random.choices(string.digits,k=6))
+    return f"BI{digits}"
+
+    
+def Bill_generated(Patient_ID):
+    Bill_ID = generate_bill_ID()
+    Patient_ID = Patient_ID
+    DATE= DATE
+    APPOINTMENT_CHARGES = APPOINTMENT_CHARGES
+    TEST_COST = TEST_COST
+    M_COST = M_COST
+    TOTAL_COST = APPOINTMENT_CHARGES+TEST_COST+M_COST
+
+    config = load_redshift_config()
+    try:
+        conn= psycopg2.connect(
+            host = config['host'],
+            port = config['port'],
+            dbname = config['dbname'],
+            user = config['user'],
+            password = config['password']
+        )
+        cursor = conn.cursor()
+        insert_bill_information = """
+        INSERT INTO user_data.Bill_Generated(Bill_ID,Patient_ID,DATE,APPOINTMENT_CHARGES,TEST_COST,M_COST,TOTAL_COST)
+        VALUES(%s,%s,%s,%s,%s,%s,%s)"""
+        
+        cursor.execute(insert_bill_information(Bill_ID,Patient_ID,DATE,APPOINTMENT_CHARGES,TEST_COST,M_COST,TOTAL_COST))
+        conn.commit()
+    except Exception as e:
+        print(f"Error loading data in redshit:{e}")
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+
